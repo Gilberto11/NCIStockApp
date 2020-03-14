d4a8d06cb704e86fdfd64959c372501685d1a264
Gilberto11 <gilbertocaobiancojunior@gmail.com>
2020-03-14 08:48:50 +0000

    finished the API stock call setup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   def index
+  	#api key required in every controller for rendering the stock data
   	@api = StockQuote::Stock.new(api_key:'pk_ed3bf1990f8a4bdcaa33dc9a9f725a7c')
+  	#if stamement for error handling as ticker is was not defined so if the form is filled user woul get an error
+  	if params[:ticker]
+  		#mapping the stock result to be based on the user input in the field we called ticker
+  		@stock = StockQuote::Stock.quote(params[:ticker]) 
+  	end
+  	
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   #get 'home/index'
   root 'home#index'
   get 'home/about'
+
+  #creating a new route to link our new button to our home/index page
+  post "/" => 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
