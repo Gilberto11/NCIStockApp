9d705271b4de336f877858d752f844dc44f87789
Gilberto11 <gilbertocaobiancojunior@gmail.com>
2020-03-14 10:33:23 +0000

    added devise db for login and register forms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,15 @@ class HomeController < ApplicationController
   	@api = StockQuote::Stock.new(api_key:'pk_ed3bf1990f8a4bdcaa33dc9a9f725a7c')
   	#error handling in case the lookup button is pressed without any value in the "ticker" field
   	if params[:ticker] == ""
-  		@empty = "You must enter a company symbol!"
+  		@empty = "You must enter a company symbol"
   	#if stamement for error handling as ticker is was not defined so if the form is filled user woul get an error
   	elsif params[:ticker]
   		#mapping the stock result to be based on the user input in the field we called ticker
   		@stock = StockQuote::Stock.quote(params[:ticker]) 
+
+      #trying to catch error if user type invalid company symbol
+      #if !@stock
+          #@error = "Company symbol does not exist, please try again!"
   	end
 
   end
