7e1a74b68955a6f03b89dd849142f4827a1fb609
Gilberto11 <gilbertocaobiancojunior@gmail.com>
2020-03-19 16:39:45 +0000

    adding more queries to the api about stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,9 @@
 
 class Stock < ApplicationRecord
+	#stock belongs to user
 	belongs_to :user
+	#validation to check if ticker field was left empty
 	validates :ticker, presence: true
+	#validation for entering invalid ticker
 	validates :ticker, format: { without: /\s/ }
 end
