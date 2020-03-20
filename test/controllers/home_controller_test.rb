9594e3109721b232fba6586a8d1459af0339f225
Gilberto11 <gilbertocaobiancojunior@gmail.com>
2020-03-20 20:26:25 +0000

    adding integration tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get home_index_url
+  test "should get homepage" do
+    get '/'
     assert_response :success
   end
+
+  test "h1 element has the text 'NCI Stock App'" do
+  	get '/'
+  	assert_select "h1", "NCI Stock App"
+  end 
+
+  test "navigation to the about page" do
+  	get "http://localhost:3000/home/about"
+  	assert_response :success
+  end
+
+  test "navigation to the Register page" do
+  	get "http://localhost:3000/users/sign_in"
+  	assert_response :success
+  end
+
 
 end
