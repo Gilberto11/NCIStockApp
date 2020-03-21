f9a3af7a1499bff65ea2c1c0da27fda3d155315a
Gilberto11 <gilbertocaobiancojunior@gmail.com>
2020-03-21 18:40:09 +0000

    one more model test added

--- test/models/stock_test.rb
+++ test/models/stock_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class StockTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     assert true
+   end
+
+   test "should not save stock without ticker" do
+   	stock = Stock.new
+  	assert_not stock.save
+end
+
 end
