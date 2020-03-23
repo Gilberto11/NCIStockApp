543a36cd365e4b43ecf3605ae03437bcfdb47caf
Gilberto11 <gilbertocaobiancojunior@gmail.com>
2020-03-23 14:01:03 +0000

    resolving email issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,15 +6,15 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
 #smtp gmail for heroku
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password:ENV["GMAIL_PASSWORD"]
-  }
+ # config.action_mailer.smtp_settings = {
+    #address: "smtp.gmail.com",
+    #port: 587,
+    #domain: ENV["GMAIL_DOMAIN"],
+    #authentication: "plain",
+    #enable_starttls_auto: true,
+    #user_name: ENV["GMAIL_USERNAME"],
+    #password:ENV["GMAIL_PASSWORD"]
+  #}
 # doesn't have to be Heroku, but you get the idea.
 #config.action_mailer.default_url_options = { :host => 'yourhostsite.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
