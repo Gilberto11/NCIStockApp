72c2084fa22877acaf7c6cc2f84518fefc1d7690
Gilberto11 <gilbertocaobiancojunior@gmail.com>
2020-03-23 14:02:41 +0000

    fixing email issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,9 @@
 Rails.application.configure do
 
-  config.action_mailer.delivery_method = :smtp
-  config.action.mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
+  #config.action_mailer.delivery_method = :smtp
+  #config.action.mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.default :charset => "utf-8"
 
 #smtp gmail for heroku
  # config.action_mailer.smtp_settings = {
