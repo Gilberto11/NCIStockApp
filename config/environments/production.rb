f915a283e5d79396ecaf44190b53ec907235a82f
Gilberto11 <gilbertocaobiancojunior@gmail.com>
2020-03-23 14:13:22 +0000

    new email attempt on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,21 @@
 Rails.application.configure do
 
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+
+config.action_mailer.smtp_settings = {
+address: "smtp.gmail.com",
+port: 587,
+domain: ENV["GMAIL_DOMAIN"],
+authentication: "plain",
+enable_starttls_auto: true,
+user_name: ENV["GMAIL_USERNAME"],
+password: ENV["GMAIL_PASSWORD"]
+}
+
   #config.action_mailer.delivery_method = :smtp
   #config.action.mailer.perform_deliveries = true
   #config.action_mailer.raise_delivery_errors = false
@@ -128,7 +144,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
   config.action_mailer.default_url_options = { :host => 'https://ncistockapp.herokuapp.com'}
   #Rails.application.routes.default_url_options[:host] = 'https://ncistockapp.herokuapp.com'
 end
